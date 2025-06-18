f56d7ba6c41cff336a754bb99ac97d346249a406
Tom Taco <28072822+wgregoryw@users.noreply.github.com>
2025-06-18 10:31:02 -0700

    Force Test

--- app.py
+++ app.py
@@ -162,6 +162,7 @@ TEMPLATE = """
 
 ARCHIVE_CACHE_FILE = "archive_playlists.json"
 ART_DIR = "static/art"
+# Force Update
 
 def save_archive_cache(data):
     with open(ARCHIVE_CACHE_FILE, "w") as f:
